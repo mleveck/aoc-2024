f443b264dd05a3e64f0a3b63f372f1cb998a0e68
mleveck <mleveck@gmail.com>
2024-11-30 22:12:10 -0800

    slight cleanup

--- day01/1.py
+++ day01/1.py
@@ -19,15 +19,9 @@ l1s = sorted(l1)
 l2s = sorted(l2)
 pairs = zip(l1s, l2s)
 diffs = [abs(p[1] - p[0]) for p in pairs]
-print(l1s)
-print(l2s)
-print(diffs)
 print(sum(diffs))
 
 # part 2
 c2 = Counter(l2)
-ans = 0
-for id in l1:
-    if id in c2:
-        ans += id * c2.get(id)
+ans = sum(id * c2.get(id, 0) for id in l1)
 print(ans)
