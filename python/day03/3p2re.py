6fd7777218cefe8f95666b3a25e19a0b1b0d7b7b
mleveck <mleveck@gmail.com>
2024-12-02 23:36:23 -0800

    no repeated lookups

--- python/day03/3p2re.py
+++ python/day03/3p2re.py
@@ -1,28 +1,28 @@
 import re
 from util import *
-from collections import Counter, deque
-from itertools import pairwise
 import sys
-from operator import mul
 
 if len(sys.argv) > 1:
     input = open(sys.argv[1]).read().strip()
 else:
     input = open("./sample.txt").read().strip()
 
-lines = input.splitlines()
 
 def process(input: str) -> int:
-    input = "do()" + input
-    mulregex = r"mul\((\d{1,3}),(\d{1,3})\)"
-    matches = re.finditer(mulregex, input)
+    mulregex = r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))"
+    matches = re.findall(mulregex, input)
     products = []
-    for match in matches:
-        mstart = match.start()
-        do_index = input.rfind("do()", 0, mstart)
-        dont_index = input.rfind("don't()", 0, mstart)
-        if do_index > dont_index:
-            products.append(int(match.group(1)) * int(match.group(2)))
+    process = True
+    for op1, op2, do, dont in matches:
+        if do:
+            process = True
+            continue
+        if dont:
+            process = False
+            continue
+        if process:
+            products.append(int(op1) * int(op2))
+
     return sum(products)
 
 
