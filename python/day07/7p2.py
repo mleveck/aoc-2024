1330560904093a70518b2c5588298f4c81da574b
Matthew Leveck <mleveck@gmail.com>
2024-12-06 22:16:28 -0800

    day 7

---
+++ python/day07/7p2.py
@@ -0,0 +1,41 @@
+from functools import cache
+from util import *
+import sys
+
+if len(sys.argv) > 1:
+    input = open(sys.argv[1]).read().strip()
+else:
+    input = open("./sample.txt").read().strip()
+
+lines = input.splitlines()
+calibrations = []
+
+
+@cache
+def can_equal(arg, rest, target):
+    if not rest:
+        return arg == target
+
+    return (
+        can_equal(arg + rest[0], rest[1:], target)
+        or can_equal(arg * rest[0], rest[1:], target)
+        or can_equal(int(f"{arg}{rest[0]}"), rest[1:], target)
+    )
+
+
+for l in lines:
+    res, args = l.split(": ")
+    res = int(res)
+    args = tuple(map(int, args.split()))
+    calibrations.append((res, args))
+
+
+ans = 0
+for target, fargs in calibrations:
+    if can_equal(fargs[0], fargs[1:], target):
+        ans += target
+
+EXPECTED = 11387
+
+print(f"expected: {EXPECTED}")
+print(f"output: {ans}")
