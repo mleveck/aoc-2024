ace6a7cc702e20a43706b2f06d4c983e3f01abf0
Matthew Leveck <mleveck@gmail.com>
2024-12-07 21:57:39 -0800

    day 8

---
+++ python/day08/8.py
@@ -0,0 +1,49 @@
+from util import *
+from collections import defaultdict
+from itertools import combinations
+import sys
+
+if len(sys.argv) > 1:
+    input = open(sys.argv[1]).read().strip()
+else:
+    input = open("./sample.txt").read().strip()
+
+lines = input.splitlines()
+
+G = parse_matrix(s=input, col_sep="")
+R = len(G)
+C = len(G[0])
+points = set()
+loc2antannae = dict()
+antenna2loc = defaultdict(list)
+
+for r, row in enumerate(G):
+    for c, char in enumerate(row):
+        cord = ord(char)
+        if 97 <= cord <= 122 or 65 <= cord <=90 or 48 <= cord <=57:
+            loc2antannae[(c, r)] = char
+            antenna2loc[char].append((c, r))
+        points.add((c, r))
+
+antinodes = set()
+
+for _, tlocs in antenna2loc.items():
+    tpairs = combinations(tlocs, 2)
+    for t1, t2 in tpairs:
+        rise = t2[1] - t1[1]
+        run = t2[0] - t1[0]
+        if (pp := point_add(t2, (run, rise))) in points:
+            antinodes.add(pp)
+
+        if (pp2 := point_add(t1, point_inv((run, rise)))) in points:
+            antinodes.add(pp2)
+
+ans = len(antinodes)
+
+
+
+
+EXPECTED = 14
+
+print(f"expected: {EXPECTED}")
+print(f"output: {ans}")
---
+++ python/day08/8p2.py
@@ -0,0 +1,48 @@
+from util import *
+from collections import defaultdict
+from itertools import combinations
+import sys
+
+if len(sys.argv) > 1:
+    input = open(sys.argv[1]).read().strip()
+else:
+    input = open("./sample.txt").read().strip()
+
+lines = input.splitlines()
+
+G = parse_matrix(s=input, col_sep="")
+points = set()
+antenna2loc = defaultdict(list)
+
+for r, row in enumerate(G):
+    for c, char in enumerate(row):
+        cord = ord(char)
+        if 97 <= cord <= 122 or 65 <= cord <=90 or 48 <= cord <=57:
+            antenna2loc[char].append((c, r))
+        points.add((c, r))
+
+antinodes = set()
+for _, tlocs in antenna2loc.items():
+    tpairs = combinations(tlocs, 2)
+    for t1, t2 in tpairs:
+        og_t1, og_t2 = t1, t2
+        rise = t2[1] - t1[1]
+        run = t2[0] - t1[0]
+
+        antinodes.add(t1)
+        while (t1 := point_add(t1, (run, rise))) in points:
+            antinodes.add(t1)
+
+        t1 = og_t1
+        while (t1 := point_add(t1, point_inv((run, rise)))) in points:
+            antinodes.add(t1)
+
+ans = len(antinodes)
+
+
+
+
+EXPECTED = 34
+
+print(f"expected: {EXPECTED}")
+print(f"output: {ans}")
